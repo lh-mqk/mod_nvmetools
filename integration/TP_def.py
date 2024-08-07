2d753be71e686d9f00d700ba7dc62066b5c77e85
Lam <quang.khai.lam@intel.com>
2024-08-07 09:05:30 +0700

    add - integration/ feature integrate script to TP

---
+++ integration/TP_def.py
@@ -0,0 +1,31 @@
+import subprocess
+import ctypes
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def executeKhaiScript():
+    try:
+        if not is_admin():
+            return False, {'Result': 'NotAdmin'}
+
+        ps_command = '''
+        while ($true) {
+            $current_time = Get-Date -Format "yyyyMMdd_HHmmss"
+            $newDir = New-Item -ItemType Directory -Path $current_time -Force
+            Push-Location $newDir
+            viewnvme
+            Pop-Location
+            Start-Sleep -Seconds 300
+        }
+        '''
+
+        subprocess.Popen(["powershell.exe", "-Command", ps_command], shell=True)
+
+        return True, {'Result': 'True'}
+
+    except Exception as e:
+        return False, {'Result': 'Exception', 'Error': str(e)}
